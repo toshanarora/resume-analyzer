4e6325642dc8fe1d3b2798d132ed1866a866029f
Toshan Arora <toshanarora2006@gmail.com>
2026-06-05 18:07:37 +0530

    Initial Spring Boot setup with HelloController

---
+++ src/test/java/com/toshan/resume_analyzer/ResumeAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toshan.resume_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
